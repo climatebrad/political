08da045ae4224956120573dc328401e09090fa98
Brad Johnson <climatebrad@gmail.com>
2019-11-22 23:55:25 -0500

    scrape image_url

--- scrapers/payne.py
+++ scrapers/payne.py
@@ -18,16 +18,19 @@ class Payne(Scraper):
         return bio.text
 
     def get_person_dict(self, person):
-        person_dict = { # name is the only standard-formatted element
-            'name' : person.find("h4").text,
-            'blurb' : person.find("p").text,
-            'bio' : self.get_payne_director_bio(person.find("a")['href'])
+        person_text = person.select_one(".et_pb_text_inner")
+        person_dict = {
+            'name' : person_text.find("h4").text,
+            'blurb' : person_text.find("p").text,
+            'bio' : self.get_payne_director_bio(person_text.find("a")['href'])
+            'image_url' : person.find("img")['src'],
         }
         return person_dict
 
+
     def get_people(self):
         people = list()
-        for person in self.soup.select(".et_pb_text_inner"):
+        for person in self.soup.select(".et_pb_column.et_pb_column_1_4"):
             if not person.find("h4"):
                 continue
             people.append(self.get_person_dict(person))
