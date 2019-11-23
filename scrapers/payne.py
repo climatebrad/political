6e9d4af15f476b1fe6a1a9af998820581e32c4da
Brad Johnson <climatebrad@gmail.com>
2019-11-23 15:08:31 -0500

    Payne Institute fellows

--- scrapers/payne.py
+++ scrapers/payne.py
@@ -21,26 +21,31 @@ class Payne(Scraper):
         self.soup = self.get_soup(self.url)
 
     def get_payne_bio(self, url):
+        """Generate bio text from the bio page. Not perfect."""
         soup = self.get_soup(url)
         bio_text = None
         if 'mines.edu' in url: # on the payne website:
+            if soup.find('h1'):
+                soup.find('h1').parent.decompose()
             bio = soup.select_one(".et_pb_text_inner")
             for strong_tag in bio.find_all('strong'):
                 strong_tag.decompose()
-            bio_text = bio.text
+            bio_text = bio.get_text(separator=' ')
         elif 'academia.edu' in url:
             bio_text = soup.select_one(".js-profile-about").text
         return bio_text
 
     def get_person_blurb(self, person):
-        tags = person_text.find_all(["p", "div"])
+        """Generate short description for person"""
+        tags = person.find_all(["p", "div"])
         return " ".join([tag.get_text(separator=' ') for tag in tags])
 
     def get_person_dict(self, person):
+        """Generate dict from person soup"""
         person_text = person.select_one(".et_pb_text_inner")
         person_dict = {
             'name' : person_text.find("h4").text,
-            'blurb' : self.get_person_blurb(person),
+            'blurb' : self.get_person_blurb(person_text),
             'bio' : self.get_payne_bio(person.find("a")['href']),
             'image_url' : person.find("img")['src'],
         }
@@ -48,6 +53,7 @@ class Payne(Scraper):
 
 
     def get_people(self):
+        """Parse the person html"""
         people = list()
         for person in self.soup.select(".et_pb_column.et_pb_column_1_4"):
             if not person.find("h4"):
