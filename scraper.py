30c221b2cc2e0f4de93728d84d4f7b1d253c44e4
Brad Johnson <climatebrad@gmail.com>
2019-11-26 07:12:24 -0500

    scrape IPCC authors

--- scraper.py
+++ scraper.py
@@ -1,15 +1,19 @@
 """scraper.py"""
 import sys
+import os.path as path
 import requests
+import json
 from bs4 import BeautifulSoup
+import pandas as pd
+from urllib.parse import urljoin
 
 class Scraper:
     def __init__(self):
         self.user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
         self.headers = {'User-Agent' : self.user_agent}
 
-    def get_json(self, url):
-        """get json from url"""
+    def get_url(self, url):
+        """get url with error checking"""
         try:
             response = requests.get(url, headers=self.headers)
             response.raise_for_status()
@@ -21,19 +25,47 @@ class Scraper:
             print ("Timeout Error:", errt)
         except requests.exceptions.RequestException as err:
             print (err)
-        return response.json()
+        return response
+
+    def get_json(self, url):
+        """get json from url"""
+        return self.get_url(url).json()
+
+    def get_table(self, url):
+        """use pandas to return list from HTML table in URL"""
+        try:
+            return pd.read_html(self.get_url(url).text)[0].to_dict('records')
+        except: # no table
+            return None
+
+    @staticmethod
+    def lowercase_keys(dict):
+        """lowercases the keys of a dict"""
+        return {key.lower(): val for key, val in dict.items()}
+
+    @staticmethod
+    def full_url(base_url, relative_href):
+        """returns full url given base_url and relative_href"""
+        return urljoin(base_url, relative_href)
+
+    @staticmethod
+    def json_load(filename):
+        """load json from filename. wraps json"""
+        with open(f'{filename}.json') as json_file:
+            return json.load(json_file)
+
+    @staticmethod
+    def json_file_exists(filename):
+        """check if filename.json exists"""
+        return path.exists(f'{filename}.json')
+
+    @staticmethod
+    def save_to_json(filename, var):
+        """write var (should be a list or dict) to json file with filename"""
+        filename = filename.lower().replace(" ","_")
+        with open(f'{filename}.json', 'w', encoding='utf-8') as json_f:
+            json.dump(var, json_f, ensure_ascii=False, indent=4)
 
     def get_soup(self, url):
         """get BeatifulSoup from url"""
-        try:
-            response = requests.get(url, headers=self.headers)
-            response.raise_for_status()
-        except requests.exceptions.HTTPError as errh:
-            print ("HTTP Error:", errh, errh.response.text)
-        except requests.exceptions.ConnectionError as errc:
-            print ("Error Connecting:", errc)
-        except requests.exceptions.Timeout as errt:
-            print ("Timeout Error:", errt)
-        except requests.exceptions.RequestException as err:
-            print (err)
-        return BeautifulSoup(response.text, 'html.parser')
+        return BeautifulSoup(self.get_url(url).text, 'html.parser')
--- scrapers/ipcc.py
+++ scrapers/ipcc.py
@@ -5,7 +5,8 @@ from scraper import Scraper
 class IPCC(Scraper):
     def __init__(self):
         super().__init__()
-        self.report_ids = self.get_report_ids()
+        self.reports_filename = 'ipcc_report_list'
+        self.reports = []
 
     def get_report_ids(self):
         """load report_ids from site."""
@@ -16,4 +17,74 @@ class IPCC(Scraper):
     def get_report_chapter_ids(self, report_id):
         """gets json in format: [ { 'id' : #, 'description' : '..', .. """
         url = 'https://apps.ipcc.ch/report/authors/json/json.chapters.php?q=' + str(report_id)
-        return { x['id'] : x['description'] for x in self.get_json(url)}
+        return [ { 'id' : x['id'],
+                   'description' : x['description'],
+                   'authors' : self.get_report_chapter_authors(report_id, x['id'])
+                  } for x in self.get_json(url) ]
+
+    def get_reports(self):
+        """gets reports with chapters as json-style list"""
+        return [ {'id' : id_,
+                  'name' : name,
+                  'chapters' : self.get_report_chapter_ids(id_)
+                 } for id_, name in self.get_report_ids().items() ]
+
+    def get_report_chapter_authors(self, report_id, chapter_id):
+        """Gets report chapter authors. Combines results of two different pages."""
+        authors_1 = self.get_report_chapter_authors_one(report_id, chapter_id)
+        authors_2 = self.get_report_chapter_authors_two(report_id, chapter_id)
+        for ix, author in enumerate(authors_1):
+            author['person']['id'] = authors_2[ix]['person']['id']
+            author['person']['photo'] = authors_2[ix]['person']['photo']
+        return authors_1
+
+    def get_report_chapter_authors_one(self, report_id, chapter_id):
+        """Gets report chapter authors. Gets gender and citizenship, but not photo and id."""
+        params = f'q={report_id}&p={chapter_id}'
+        url = f'https://apps.ipcc.ch/report/authors/report.authors.php?{params}'
+        authors = self.get_table(url)
+        if authors: # there may not be any authors
+            for ix, author in enumerate(authors):
+                person = self.lowercase_keys(author)
+                person['last name'] = person['last name'].title()
+                authors[ix] = {
+                    'index' : int(person.pop('unnamed: 0', None)) - 1,
+                    'role' : person.pop('role', None),
+                    'person' : person
+                }
+        else:
+            authors = []
+        return authors
+
+    def get_report_chapter_authors_two(self, report_id, chapter_id):
+        """Gets report chapter authors. gets photo and id, but not gender and citizenship"""
+        params = f'q={report_id}&p={chapter_id}'
+        url = f'https://apps.ipcc.ch/report/authors/authors.php?{params}'
+        soup = self.get_soup(url)
+        authors = []
+        for ix, author in enumerate(soup.select('.mix')):
+            last, first = author.select_one('span.author').text.title().split(', ')
+            affiliation = author.find('font')
+            authors.append({
+                'index' : ix,
+                'role' : author['data-role'],
+                'person' : {
+                    'id': int(author.select_one('span.author')['data-cid']),
+                    'last name': last,
+                    'first name': first,
+                    'country': affiliation.contents[2],
+                    'current affiliation': affiliation.contents[0],
+                    'photo': self.full_url(url, author.find('img')['src']) }
+            })
+        return authors
+
+    def save_report_json(self):
+        """scrape and ipcc_report_list.json file"""
+        reports = self.get_reports()
+        self.save_to_json(self.reports_filename, reports)
+
+    def load_reports(self, reload=False):
+        """looks for saved json file unless reload is true, then scrapes from IPCC"""
+        if reload or (not self.json_file_exists(self.reports_filename)):
+            self.save_report_json()
+        self.reports = self.json_load(self.reports_filename)
